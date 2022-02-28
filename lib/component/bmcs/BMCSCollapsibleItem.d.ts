970e4d3f302cc661128e6cd76745ca9d4e78a3c0
Hanul <hanul@hanul.me>
2022-02-28 13:17:35 +0900

    build

---
+++ lib/component/bmcs/BMCSCollapsibleItem.d.ts
@@ -0,0 +1,6 @@
+import { DomNode } from "@hanul/skynode";
+export default class BMCSCollapsibleItem extends DomNode {
+    private content;
+    constructor(title: string, desc: string);
+}
+//# sourceMappingURL=BMCSCollapsibleItem.d.ts.map
---
+++ lib/view/Bmcs.d.ts
@@ -0,0 +1,8 @@
+import { View, ViewParams } from "skyrouter";
+export default class Bmcs implements View {
+    private container;
+    constructor();
+    changeParams(params: ViewParams, uri: string): void;
+    close(): void;
+}
+//# sourceMappingURL=Bmcs.d.ts.map
