bdcbf7ff386bebfb2c7ba5f5645f6c5fe096fe4e
dilrong <dilrong@dilrong.com>
2022-03-15 01:11:08 +0900

    feat: a.bmcs

--- src/view/Layout.ts
+++ src/view/Layout.ts
@@ -29,6 +29,7 @@ export default class Layout implements View {
                     el("img.logo", { align: "left", src: "/images/logo.svg", alt: "logo" }),
                 ),
                 new PCMenu(),
+                el("a.bmcs", { click: () => ViewUtil.go("bmcs") }, "Go to BMCS >"),
                 select = el("select.language-select",
                     el("option", "🇰🇷 KOREAN", { value: "ko" }),
                     // el("option", "🇺🇸 ENG", { value: "en" }),
